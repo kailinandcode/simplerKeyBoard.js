3e7673ee31d4023b4251ae090390d87dcc529bf4
Kailin Li <35666977+kailinandcode@users.noreply.github.com>
2018-02-13 22:51:43 -0800

    Update simpleKeyboard1.js

--- simpleKeyboard1.js
+++ simpleKeyboard1.js
@@ -1,8 +1,13 @@
+/*CREATES A 500x200 canvas, where background color, text size, and location of ellipse
+depend on the frequency of the key you pressed
+- Kailin, 2/13 10:51pm */
+
 var freqA = 174;
 var freqS = 196;
 var freqD = 220;
 var freqF = 246;
 
+//add a default frequency variable that's not freqA, freqS, freqD or freqF
 var frequency = 150;
 
 var oscA, oscS, oscD, oscF;
@@ -10,7 +15,9 @@ var oscA, oscS, oscD, oscF;
 var playing = false;
 
 function setup() {
+  //background color has been changed
   backgroundColor = color(255, 200, 25);
+  //creates a new 500 x 200 canvas; change the text size to accomodate
   createCanvas(500, 200);
   textSize(24);
   textAlign(CENTER);
@@ -42,15 +49,19 @@ function setup() {
 
 function draw() {
   background(backgroundColor);
+  //vary the text size by frequency
   textSize(frequency - 125);
   text('click to play', width / 2, height / 2);
+  //creating four rectangles, that resemble the four black keys on the keyboard
   noStroke();
   fill(0);
   rect(240, 140, 10, 60);
   rect(220, 140, 10, 60);
   rect(260, 140, 10, 60);
   rect(280, 140, 10, 60);
-  if (frequency != 100) {
+  /*if the frequency is not the default, then we create an ellipse whose color and location
+  depend on the key's frequency*/
+  if (frequency != 150) {
     strokeWeight(3);
     stroke(0);
     fill(frequency - 100);
@@ -79,6 +90,7 @@ function keyPressed() {
   if (osc) {
     osc.amp(0.5, 0.2);
     playing = true;
+    //change background color when pressed to be frequency-related
     backgroundColor = color(freq, freq-10, 150);
   }
 }
@@ -103,6 +115,7 @@ function keyReleased() {
   if (osc) {
     osc.amp(0, 0.2);
     playing = false;
+    //change background color when released to be frequency-related
     backgroundColor = color(freq, freq, 75);
   }
 }
